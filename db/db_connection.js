6f6340b02fc9ec0381066088a891ee3532b3c3c1
Anjan GHosh <122563582+anjan-cs98@users.noreply.github.com>
2024-01-22 12:23:04 +0530

    Update db_connection.js

--- db/db_connection.js
+++ db/db_connection.js
@@ -1,10 +1,5 @@
 /**Loading Mongoose **/
 const mongoose = require('mongoose');
-
-// /**Build Data Base connection String **/
-// const db_url = 'mongodb://127.0.0.1:27017/Foodrestro';
-const db_url = 'mongodb+srv://anjan:a123@food-rest-api.inuqoav.mongodb.net/Foodrestro';
-
 /**lets connect mongodb data base **/
 const con = mongoose.connect(db_url)
        .then(() => {
